e1996ca6b6a80b558b72ea4357893027159d6d65
webukatu <info@webukatu.com>
2018-08-31 23:45:40 +0900

    Revert "reactでタスクリストコンポーネントを実装"
    This reverts commit b4ca2b3479febb3e208e2d8e13590b745f4bb0a9.

--- react_sample01-05-redux/src/js/components/TodoList.js
+++ react_sample01-05-redux/src/js/components/TodoList.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import Task from './Task';
+import _ from 'lodash';
 
-export default class TodoList extends React.Component {
+export class TodoList extends React.Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      data: [
+        {
+          id: 0,
+          text: 'sample todo1'
+        },
+        {
+          id: 1,
+          text: 'sample todo2'
+        }
+      ]
+    };
     this.handleRemove = this.handleRemove.bind(this);
   }
   handleRemove(id){
-    this.props.callBackRemoveTask(id);
+    // for in などでループしてidをチェックしてもいいが、lodashで簡単にできる
+    // let data = [];
+    // for(let i in this.state.data){
+    //   if(this.state.data[i].id !== id){
+    //     data.push(this.state.data[i]);
+    //   }
+    // }
+    let data = _.reject(this.state.data, { 'id': id });
+    this.setState({
+      data: data
+    });
   }
   render() {
+
     let tasks = [];
-    for(let i in this.props.data){
-      tasks.push(<Task key={this.props.data[i].id}
-                       id={this.props.data[i].id}
-                       text={this.props.data[i].text} onRemove={this.handleRemove} />);
+    for(let i in this.state.data){
+      // コンポーネントをループで複数生成する場合は、keyを指定する必要がある。
+      // keyはReactがコンポーネントを一意に識別するためのもの
+      // keyにはiかidを指定することが一般的
+      tasks.push(<Task key={this.state.data[i].id}
+                       id={this.state.data[i].id}
+                       text={this.state.data[i].text} onRemove={this.handleRemove} />);
     }
 
     return (
